890bfa8ad22aa1b5c7805e57831c8ed9786393a4
Maxim Dubinin <maxim.dubinin@nextgis.com>
2019-02-07 19:30:59 +0300

    other params

--- runmx.py
+++ runmx.py
@@ -5,9 +5,8 @@ import platform
 import random
 import datetime
 import string
-from osgeo import ogr
-from osgeo import osr
 import csv
+import glob
 
 #python runmx.py
 
@@ -17,7 +16,7 @@ import csv
 #python runmx.py --input samples\bradypus.csv --env layers --features linear,quadratic --of logistic
 
 #Full example:
-#python runmx.py --input samples\bradypus.csv --env layers --features linear,quadratic,product,threshold,hinge --of logistic --curves --jack
+#python runmx.py --input samples\bradypus.csv --env layers --features linear,quadratic,product,threshold,hinge --of logistic --curves --jack --maxit 600
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--input',type=str,required=True)
@@ -32,9 +31,9 @@ parser.add_argument('--max',type=int)
 parser.add_argument('--rep',type=int)
 parser.add_argument('--reptype',type=str,choices=['cross','boot','subsample'])
 parser.add_argument('--noadds',action="store_true")
-parser.add_argument('--maxit',type=str)
-parser.add_argument('--prev',type=str)
-parser.add_argument('--thr',type=str)
+parser.add_argument('--maxit',type=int)
+parser.add_argument('--prev',type=int)
+parser.add_argument('--thr',type=str,choices=['fix1','fix5','fix10','min','perc10','equaltrain','maxtrain','equaltest','maxtest','equate'])
 parser.add_argument('--bias',type=str)
 parser.add_argument('--env',type=str,required=True)
 parser.add_argument('--envno',type=str)
@@ -63,6 +62,11 @@ def prepare_params():
     if not os.path.exists(env): 
         print('Environmental variables folder does not exist. Exiting.')
         sys.exit(1)
+    
+    envcat = ''
+    for l in glob.glob(os.path.join(env,'*.asc')):
+        if os.path.exists(l + '.cat'):
+            envcat = envcat + ' -t ' + l.replace('.asc','').replace(env,'').replace('\\','')
     
     feat = ' '
     if args.features:
@@ -113,9 +117,43 @@ def prepare_params():
     noadds = ''
     if args.noadds: noadds = ' noaddsamplestobackground'
     
-    return env,input,output,feat,of,curves,jack,rnd,reg,max,rep,reptype,rndseed,noadds
+    maxit = ''
+    if args.maxit: maxit = ' maximumiterations=' + str(args.maxit)
+    
+    prev = ''
+    if args.prev: prev = ' defaultprevalence=' + str(args.prev)
+    
+    thr = ' '
+    if args.thr:
+        if args.thr == 'fix1': thr = ' "applythresholdrule=fixed cumulative value 1" '
+        if args.thr == 'fix5': thr = ' "applythresholdrule=fixed cumulative value 5" '
+        if args.thr == 'fix10': thr = ' "applythresholdrule=fixed cumulative value 10" '
+        if args.thr == 'min': thr = ' "applythresholdrule=minimum training presence" '
+        if args.thr == 'perc10': thr = ' "applythresholdrule=10 percentile training presence" '
+        if args.thr == 'equaltrain': thr = ' "applythresholdrule=equal training sensitivity and specificity" '
+        if args.thr == 'maxtrain': thr = ' "applythresholdrule=maximum training sensitivity plus specificity" '
+        #Next two are not recognized http://m-d.me/img/ss/20190207_165622.png
+        if args.thr == 'equaltest': 
+            thr = ' "applythresholdrule=????" '
+            print('Error: Threshold rule equal test sensitivity and specificity not recognized')
+            sys.exit(1)
+        if args.thr == 'maxtest': 
+            thr = ' "applythresholdrule=????" '
+            print('Error: Threshold rule maximum test sensitivity plus specificity not recognized')
+            sys.exit(1)
+        if args.thr == 'equate': thr = ' "applythresholdrule=equate entropy of thresholded and original distributions" '
+    
+    envno = ' '
+    if args.envno:
+        for envn in args.envno.split(','):
+            envno = envno + ' -N ' + envn
+    
+    return env,envcat,input,output,feat,of,curves,jack,rnd,reg,max,rep,reptype,rndseed,noadds,maxit,prev,thr,envno
 
 def reproject_input(input,output):
+    from osgeo import ogr
+    from osgeo import osr
+
     source = osr.SpatialReference()
     source.ImportFromEPSG(4326)
 
@@ -143,7 +181,7 @@ def reproject_input(input,output):
                 output_writer.writerow([species, x, y])
         
     
-def run(env,input,output,feat,of,curves,jack,rnd,reg,max,rep,reptype,rndseed,noadds):
+def run(env,envcat,input,output,feat,of,curves,jack,rnd,reg,max,rep,reptype,rndseed,noadds,maxit,prev,thr,envno):
     params_str = 'environmentallayers=%s samplesfile=%s outputdirectory=%s' % (env,input,output)
     
     if feat != '':
@@ -177,6 +215,21 @@ def run(env,input,output,feat,of,curves,jack,rnd,reg,max,rep,reptype,rndseed,noa
     if noadds != '':
         params_str = params_str + noadds
     
+    if maxit != '':
+        params_str = params_str + maxit
+        
+    if prev != '':
+        params_str = params_str + prev
+        
+    if thr != '':
+        params_str = params_str + thr
+        
+    if envno != '':
+        params_str = params_str + envno
+        
+    if envcat != '':
+        params_str = params_str + envcat
+        
     cmd = 'java -mx512m -jar ' + maxbin + ' ' + params_str + ' redoifexists autorun' 
     print cmd
     os.system(cmd)
@@ -190,5 +243,5 @@ if __name__ == '__main__':
 
     maxbin = os.path.join('bin','maxent.jar')
 
-    env,input,output,feat,of,curves,jack,rnd,reg,max,rep,reptype,rndseed,noadds = prepare_params()
-    run(env,input,output,feat,of,curves,jack,rnd,reg,max,rep,reptype,rndseed,noadds)
+    env,envcat,input,output,feat,of,curves,jack,rnd,reg,max,rep,reptype,rndseed,noadds,maxit,prev,thr,envno = prepare_params()
+    run(env,envcat,input,output,feat,of,curves,jack,rnd,reg,max,rep,reptype,rndseed,noadds,maxit,prev,thr,envno)
